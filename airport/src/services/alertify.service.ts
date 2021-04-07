ae1aa2c695605e5be93108fc820213f2b55989b8
matzaj1 <matzaj312@gmail.com>
2021-04-07 10:51:43 +0200

    Add alertify service

---
+++ airport/src/services/alertify.service.ts
@@ -0,0 +1,22 @@
+import * as alertify from 'alertifyjs';
+
+export class AlertifyService {
+
+  constructor() { }
+
+  success(message: string): void {
+    alertify.success(message);
+  }
+
+  error(message: string): void {
+    alertify.error(message);
+  }
+
+  warning(message: string): void {
+    alertify.warning(message);
+  }
+
+  message(message: string): void {
+    alertify.message(message);
+  }
+}
