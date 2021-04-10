acbeaf27297a6eaa2790695430c0003a378af8d7
Adam <adamo.b1997@gmail.com>
2021-04-10 20:45:48 +0200

    Add dependencies + assets

--- airport/src/app/app.module.ts
+++ airport/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ToastrModule} from 'ngx-toastr';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FlightRowComponent } from './components/home/flight-row/flight-row.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DatePipe} from '@angular/common';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopBarComponent,
+    FlightRowComponent
   ],
   imports: [
     BrowserModule,
@@ -15,8 +29,13 @@ import {ToastrModule} from 'ngx-toastr';
       progressBar: true,
       progressAnimation: 'increasing'
     }),
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- airport/src/environments/environment.ts
+++ airport/src/environments/environment.ts
@@ -3,8 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'http://localhost:5000/api'
 };
+
+export enum ApiPaths {
+  Auth = '/auth',
+  AirportApi = '/airport',
+  Airplane = '/airplane',
+  Flight = '/flight'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
