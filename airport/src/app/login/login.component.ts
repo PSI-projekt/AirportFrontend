30e562717672da87416ee37aa89d74c73955d931
matzaj1 <matzaj312@gmail.com>
2021-04-14 14:23:56 +0200

    Add auth requests

--- airport/src/app/login/login.component.ts
+++ airport/src/app/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
+import { AuthService } from '../api/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService, private toastr: ToastrService) { }
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const username = form.value.username;
+    const password = form.value.password;
+
+    this.authService.login(username, password).subscribe(
+      resData => {
+        console.log(resData);
+        this.toastr.success('Your are now logged in!', 'Success!');
+      },
+      errorMessasge => {
+        console.log(errorMessasge);
+        this.toastr.error(errorMessasge, 'Error');
+      }
+    );
   }
-
 }
--- airport/src/app/register/register.component.ts
+++ airport/src/app/register/register.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+import { AuthService } from '../api/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService, private toastr: ToastrService) { }
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const username = form.value.username;
+    const password = form.value.password;
+    const email = form.value.email;
+
+    this.authService.register(username, password, email).subscribe(
+      resData => {
+        console.log(resData);
+        this.toastr.success('Your account has been created!', 'Success!');
+      },
+      errorMessasge => {
+        console.log(errorMessasge);
+        this.toastr.error(errorMessasge,'Error');
+      }
+     );
   }
-
 }
