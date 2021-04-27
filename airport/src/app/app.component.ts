5d6b88bca74a0775822264ed40b71ae7d1cb3d89
Adam <adamo.b1997@gmail.com>
2021-04-27 10:18:25 +0200

    Update AppComponent

--- airport/src/app/app.component.ts
+++ airport/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {Platform} from '@angular/cdk/platform';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(public router: Router) {
+export class AppComponent implements OnInit {
+  public shouldBeDropdown = false;
+
+  constructor(public router: Router, public platform: Platform) {
+  }
+
+  ngOnInit(): void {
+    this.resizeScreen();
+  }
+
+  public resizeScreen(): void {
+    const screenWidth = window.innerWidth;
+    this.shouldBeDropdown = screenWidth <= 1000 || this.platform.ANDROID || this.platform.IOS;
   }
 }
