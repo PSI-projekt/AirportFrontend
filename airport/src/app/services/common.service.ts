848312d89da9b5fa3394f4ba3c72e7d505ec1892
Adam <adamo.b1997@gmail.com>
2021-05-21 11:45:42 +0200

    Update common.service.ts

--- airport/src/app/services/common.service.ts
+++ airport/src/app/services/common.service.ts
@@ -8,15 +8,21 @@ import {PaymentDto} from '../api/dtos/payment.dto';
 })
 export class CommonService {
   public lastPage: number;
+  public lastPageBookings: number;
   public selectedFlight: FlightDto | undefined;
   public passengers: Array<PassengerForListDto> | undefined;
   public paymentDetails: PaymentDto | undefined;
 
   constructor() {
     this.lastPage = 1;
+    this.lastPageBookings = 1;
   }
 
   public setLastPage(page: number | undefined): void {
     this.lastPage = page === undefined ? 1 : page;
+  }
+
+  public setLastPageBookings(page: number | undefined): void {
+    this.lastPageBookings = page === undefined ? 1 : page;
   }
 }
