28407221a5fd922f222e401d46e1756387f1a714
Adam <adamo.b1997@gmail.com>
2021-05-21 11:45:14 +0200

    Update BookingService + Add DTOs + Update pagiantion

--- airport/src/app/api/booking.service.ts
+++ airport/src/app/api/booking.service.ts
@@ -6,6 +6,8 @@ import {BookingForAddDto} from './dtos/booking-for-add.dto';
 import {Observable} from 'rxjs';
 import {PaymentDto} from './dtos/payment.dto';
 import {map} from 'rxjs/operators';
+import {BookingForListDto} from './dtos/booking-for-list.dto';
+import {PaginatedResult} from './dtos/pagination';
 
 @Injectable({
   providedIn: 'root'
@@ -26,5 +28,40 @@ export class BookingService {
 
     return this.http.post<PaymentDto>(this.url, bookingForAdd, { headers }).pipe(
       map((response: PaymentDto) => response));
+  }
+
+  public getBookings(page: number, itemsPerPage: number): Observable<PaginatedResult<Array<BookingForListDto>>> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.authService.getToken()}`
+    });
+
+    const parameters = {
+      pageNumber: page.toString(),
+      pageSize: itemsPerPage.toString()
+    };
+
+    const url = this.url + '/bookings';
+
+    return this.http.get<any>(url, { headers, params: parameters, observe: 'response'})
+      .pipe(map((response: any) => {
+        const result = new PaginatedResult<Array<BookingForListDto>>();
+        result.result = response.body;
+        if (response.headers.get('pagination') !== null) {
+          result.pagination = JSON.parse(response.headers.get('pagination') as string);
+        }
+        return result;
+      }));
+  }
+
+  public getPdf(bookingId: number): Observable<Blob> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.authService.getToken()}`
+    });
+
+    const url = this.url + '/pdf/' + bookingId;
+
+    return this.http.get(url, {responseType: 'blob', headers});
   }
 }
---
+++ airport/src/app/api/dtos/passenger-for-edit.dto.ts
@@ -0,0 +1,11 @@
+export interface PassengerForEditDto {
+  id: number;
+  firstName: string;
+  lastName: string;
+  country: string;
+  city: string;
+  street: string;
+  streetNumber: string;
+  postCode: string;
+  idNumber: string;
+}
