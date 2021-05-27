e78c4990c97ec3fc94c81a212d3cec6dbc76c519
blazejzaj <62843011+blazejzaj@users.noreply.github.com>
2021-05-27 19:16:50 +0200

    Update airport.service.ts

--- airport/src/app/api/airport.service.ts
+++ airport/src/app/api/airport.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
-import {ApiPaths, environment} from '../../environments/environment';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {AirportCountDto} from './dtos/airport-count.dto';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {catchError, map} from 'rxjs/operators';
+import {Observable, ReplaySubject, throwError} from 'rxjs';
+import { ApiPaths, environment } from '../../environments/environment';
+import { UserForLoginDto } from './dtos/user-for-login-dto';
+import {User} from '../interfaces/user';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {ToastrService} from 'ngx-toastr';
+import { AuthService } from './auth.service';
+import { AirportForEditDto } from './dtos/airport-for-edit.dto';
+import { AirportForListDto } from './dtos/airport-for-list.dto';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AirportService {
 
   private readonly url: string;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private toastr: ToastrService, private authService: AuthService) {
     this.url = environment.baseUrl + ApiPaths.AirportApi;
   }
 
@@ -21,5 +28,43 @@ export class AirportService {
 
     return this.http.get<AirportCountDto>(url).pipe(
       map((responseData: AirportCountDto) => responseData));
+  }
+
+  public getAirportList(): Observable<Array<AirportForListDto>> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.authService.getToken()}`
+    });
+
+    return this.http.get<AirportForListDto>(this.url, { headers }).pipe(
+      map((responseData: AirportForListDto) => responseData));
+  }
+
+  public edit(airportForEdit: AirportForEditDto): Observable<AirportForEditDto> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.authService.getToken()}`
+    });
+
+    return this.http.patch<AirportForEditDto>(this.url, airportForEdit, { headers })
+      .pipe(catchError(this.handleError)
+    );
+  }
+
+  private handleError(errorRes: HttpErrorResponse): Observable<any> {
+    let errorMessage = 'An unknown error occurred';
+
+    switch (errorRes.status) {
+      case 500:
+        errorMessage = 'The server error occurred';
+        break;
+      case 400:
+        errorMessage = errorRes.error;
+        break;
+      case 401:
+        errorMessage = 'You are not allowed to do this';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
