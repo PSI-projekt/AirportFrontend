adb951547fa94507cd3ab67382ab39e908a66395
Adam <adamo.b1997@gmail.com>
2021-06-08 10:19:48 +0200

    Update AirplaneService + Add DTO

--- airport/src/app/api/airplane.service.ts
+++ airport/src/app/api/airplane.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {ApiPaths, environment} from '../../environments/environment';
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {AirplaneForListDto} from './dtos/airplane-for-list.dto';
-import {map} from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
 import {AuthService} from './auth.service';
+import {AirplaneForAddDto} from "./dtos/airplane-for-add.dto";
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,31 @@ export class AirplaneService {
 
     return this.http.get<Array<AirplaneForListDto>>(this.url, { headers }).pipe(
       map((responseData: Array<AirplaneForListDto>) => responseData));
+  }
+
+  public CreateAirplane(airplane: AirplaneForAddDto): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.authService.getToken()}`
+    });
+
+    return this.http.post(this.url, airplane, {headers}).pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse): Observable<any> {
+    let errorMessage = 'An unknown error occurred';
+
+    switch (errorRes.status) {
+      case 500:
+        errorMessage = 'The server error occurred';
+        break;
+      case 400:
+        errorMessage = errorRes.error;
+        break;
+      case 401:
+        errorMessage = 'Wrong username or password';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
