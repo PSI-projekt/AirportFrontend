262c5c58e48e1395c53f36c9a9573a8f8a7fbdb3
blazejzaj <62843011+blazejzaj@users.noreply.github.com>
2021-06-11 02:19:31 +0200

    Create airport-add.component.ts

---
+++ airport/src/app/components/management/airport-add/airport-add.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../../../api/auth.service';
+import {ToastrService} from 'ngx-toastr';
+import {User} from '../../../interfaces/user';
+import {FormGroup, NgForm} from "@angular/forms";
+import {AirportForAddDto} from "../../../api/dtos/airport-for-add.dto";
+import {AirportService} from "../../../api/airport.service";
+import {Privileges} from "../../../enums/privileges.enum";
+
+@Component({
+  selector: 'app-airport-add',
+  templateUrl: './airport-add.component.html',
+  styleUrls: ['./airport-add.component.css']
+})
+export class AirportAddComponent implements OnInit {
+  public user: User | undefined;
+  public isFetching = false;
+
+  constructor(public router: Router, private authService: AuthService, private toastr: ToastrService,
+              private airportService: AirportService) {
+    this.authService.currentUser$.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  ngOnInit(): void {
+    this.redirect();
+  }
+
+  private redirect(): void {
+    if (this.user === undefined || this.user.role === Privileges.applicationUser) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  public onSubmit(airportForm: NgForm): void {
+    const airport: AirportForAddDto = {
+      name: airportForm.value.name,
+      country: airportForm.value.country,
+      city: airportForm.value.city,
+      street: airportForm.value.street,
+      codeIATA: airportForm.value.codeIATA
+    };
+
+    this.isFetching = true;
+
+    this.airportService.addAirport(airport).subscribe(() => {
+      this.toastr.success('Successfully added airport', 'Success');
+      this.isFetching = false;
+    }, errorMsg => {
+      this.toastr.error(errorMsg, 'Error');
+      this.isFetching = false;
+    });
+  }
+}
